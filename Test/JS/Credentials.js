fbdd1f1fa33e1c06e13cc4e3b0513f43cf6f34be
Dave Retterer <d-retterer@onu.edu>
2016-02-14 13:21:18 -0500

    Preparing for use by students.

--- Test/JS/Credentials.js
+++ Test/JS/Credentials.js
@@ -1,7 +1,8 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * By: Prof. Dave Retterer
+ * Comment out various portions of code to illustrate different
+ * ways to approach the problem of validating username/password
+ * credentials and of indication the result of the validation.
  */
 
 function ValidateCredentials()
@@ -10,23 +11,23 @@ function ValidateCredentials()
     var Username = document.getElementById("username").value;
     var Password = document.getElementById("password").value;
     
-/*
+/**/
     if (Username == "Dave" && Password == "Retterer")
         alert("Valid Credentials -- You are logged in.");
     else
         alert("Invalid Credentials -- You are guilty of attempted tresspassing");
-*/
+
 
 /*    
     ValidateCredentials2(Username, Password);
 */
 
-/* */  
+/*  
     if (ValidateCredentials3(Username,Password))
         NotifyLoginSuccessful();
     else
         NotifyLoginFailed();
-
+*/
 }
 
 function ValidateCredentials2(UN, PWD)
@@ -50,7 +51,9 @@ function ValidateCredentials3(UN, PWD)
         
     var i;
     
-    alert(ValidCredentials);
+    //  To see what is in the array above at run time uncomment the line below:
+    //  alert(ValidCredentials);  
+    
     for (i=0; i<ValidCredentials.length;i++)
     {
         
@@ -65,10 +68,35 @@ function ValidateCredentials3(UN, PWD)
 
 function NotifyLoginSuccessful()
 {
-    alert("Valid Credentials -- You are logged in :-)");
+    // This is a lousy way to indicate that the user is logged in but
+    // is being used here just to show where that indication can be 
+    // done.  See other code sections below for other ways to do it.
+    
+     alert("Valid Credentials -- You are logged in :-)");
+    
+    /*
+    // This is a more meaningful way to indicate that the user is logged in
+    // successfully -- though it is somewhat annoying.
+    
+    var cred = document.getElementById("Credentials");
+    cred.style.backgroundColor = "#dcf2e0";
+    */
+  
 }
 
 function NotifyLoginFailed()
 {
-    alert("Invalid Credentials -- You are guilty of attempted tresspassing :-(");
+   // This is a lousy way to indicate that the user is logged in but
+   // is being used here just to show where that indication can be 
+   // done.  See other code sections below for other ways to do it.
+   
+  alert("Invalid Credentials -- You are guilty of attempted tresspassing :-(");
+  /* 
+   // This is a more meaningful way to indicate that the user is logged in
+   // successfully -- though it is somewhat annoying.
+   var cred = document.getElementById("Credentials");
+   cred.style.backgroundColor = "#f2dce0";
+  */ 
+    
 }
+

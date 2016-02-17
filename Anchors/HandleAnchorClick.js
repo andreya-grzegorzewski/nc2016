6780cc9f29ac62c88d86659999d6cdfd50e2aa56
Dave Retterer <d-retterer@onu.edu>
2016-02-17 11:45:11 -0500

    Changes from class 2/17/16

--- Anchors/HandleAnchorClick.js
+++ Anchors/HandleAnchorClick.js
@@ -6,17 +6,27 @@
 
 function init()
 {
+    alert("In init()");
     document.body.style.textAlign = "center";
     document.body.style.border = "10px solid green";
 }
 
 function theAnchorOnClick(ItemNum)
 {
+    
     document.getElementById("divMenuItem1").style.display = "none";
     document.getElementById("divMenuItem2").style.display = "none";
     document.getElementById("divMenuItem3").style.display = "none";
-    
     document.getElementById("divMenuItem"+ItemNum).style.display = "block";
+   
     
+    /*
+    alert("Hello");
+    document.getElementById("divMenuItem1").style.visibility = "hidden";
+    document.getElementById("divMenuItem2").style.visibility = "hidden";
+    document.getElementById("divMenuItem3").style.visibility = "hidden";
+  
+    document.getElementById("divMenuItem"+ItemNum).style.visibility = "visible";
     
+    */
 }
